210bea21503bae8453c31fccf8e456ef842adee4
nbeisenb <nbeusenbek@mail.ru>
2024-10-27 16:59:29 +0500

    server

--- main.go
+++ main.go
@@ -6,50 +6,47 @@ import (
 	"log"
 	"net/http"
 	"os"
-	"path/filepath"
 )
 
-var dataDirectory string
+// func createBucket(w http.ResponseWriter, r *http.Request) {
+// 	bucketName := r.PathValue("BucketName")
 
-func createBucket(w http.ResponseWriter, r *http.Request) {
-	bucketName := r.PathValue("BucketName")
+// 	if err := isValidBucketName(bucketName); err != nil {
+// 		w.WriteHeader(http.StatusBadRequest)
+// 		return
+// 	}
 
-	if err := isValidBucketName(bucketName); err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
+// 	// Check if bucket already exists
+// 	if bucketExists(bucketName) {
+// 		w.WriteHeader(http.StatusConflict)
+// 		return
+// 	}
 
-	// Check if bucket already exists
-	if bucketExists(bucketName) {
-		w.WriteHeader(http.StatusConflict)
-		return
-	}
+// 	// Create the bucket directory
+// 	bucketDir := filepath.Join(dataDirectory, bucketName)
+// 	if err := os.MkdirAll(bucketDir, os.ModePerm); err != nil {
+// 		fmt.Printf("failed to create bucket directory: %w", err)
+// 		return
+// 	}
 
-	// Create the bucket directory
-	bucketDir := filepath.Join(dataDirectory, bucketName)
-	if err := os.MkdirAll(bucketDir, os.ModePerm); err != nil {
-		fmt.Printf("failed to create bucket directory: %w", err)
-		return
-	}
+// 	// Save bucket metadata
+// 	if err := saveBucketMetadata(bucketName); err != nil {
+// 		fmt.Printf("failed to save bucket metadata: %w", err)
+// 		return
+// 	}
 
-	// Save bucket metadata
-	if err := saveBucketMetadata(bucketName); err != nil {
-		fmt.Printf("failed to save bucket metadata: %w", err)
-		return
-	}
-
-	fmt.Printf("Bucket '%s' created successfully.\n", bucketName)
-}
+// 	fmt.Printf("Bucket '%s' created successfully.\n", bucketName)
+// }
 
 func main() {
 	port := flag.String("port", "8080", "Port for th HTTP server")
-	dataDirectory = *flag.String("dir", "./data", "Directory for storing files")
+	dir := flag.String("dir", "./data", "Directory for storing files")
 	flag.Parse()
 
 	//creating directory if it is not exist
-	_, err := os.Stat(dataDirectory)
+	_, err := os.Stat(*dir)
 	if os.IsNotExist(err) {
-		err = os.Mkdir(dataDirectory, os.ModePerm)
+		err = os.Mkdir(*dir, os.ModePerm)
 		if err != nil {
 			log.Fatalf("Could nor create data directoty: %v", err)
 		}
@@ -57,10 +54,14 @@ func main() {
 		log.Fatalf("Error checking directory: %v", err)
 	}
 
-	StartServer(*port) //starting server
-
-	http.HandleFunc("PUT /{BucketName}", createBucket)
+	//http.HandleFunc("PUT /{BucketName}", createBucket)
 	http.HandleFunc("/status", statusHandler)
+
+	fmt.Printf("Server is running on port %s\n", *port)
+	err = http.ListenAndServe(":"+*port, nil)
+	if err != nil {
+		log.Fatalf("Could not start the server: %v", err)
+	}
 }
 
 func statusHandler(w http.ResponseWriter, r *http.Request) {
