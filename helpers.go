8e4ee5767583f76d1085191d012347e99cb4f6bc
nbeisenb <nbeusenbek@mail.ru>
2024-10-27 16:49:03 +0500

    bucket-beginnig

--- helpers.go
+++ helpers.go
@@ -1,21 +1,23 @@
 package main
 
 import (
+	"encoding/csv"
 	"errors"
+	"fmt"
+	"os"
 	"strings"
+	"time"
 	"unicode"
 )
 
-// isValidBucketName checks if the bucket name meets the specified naming conventions without using regexp.
 func isValidBucketName(name string) error {
-	// Check length constraints
+
 	if len(name) < 3 || len(name) > 63 {
 		return errors.New("bucket name must be between 3 and 63 characters")
 	}
 
-	// Check allowed characters and consecutive periods/dashes
 	prevChar := rune(0)
-	for i, ch := range name {
+	for _, ch := range name {
 		if !unicode.IsLower(ch) && !unicode.IsDigit(ch) && ch != '-' && ch != '.' {
 			return errors.New("bucket name can only contain lowercase letters, numbers, dots, and hyphens")
 		}
@@ -25,12 +27,10 @@ func isValidBucketName(name string) error {
 		prevChar = ch
 	}
 
-	// Check if name is formatted as an IP address
 	if isIPAddress(name) {
 		return errors.New("bucket name must not be formatted as an IP address")
 	}
 
-	// Check start and end constraints
 	if name[0] == '-' || name[len(name)-1] == '-' {
 		return errors.New("bucket name must not begin or end with a hyphen")
 	}
@@ -38,7 +38,6 @@ func isValidBucketName(name string) error {
 	return nil
 }
 
-// isIPAddress checks if the name follows the format of an IP address.
 func isIPAddress(name string) bool {
 	parts := strings.Split(name, ".")
 	if len(parts) != 4 {
@@ -55,4 +54,46 @@ func isIPAddress(name string) bool {
 		}
 	}
 	return true
+}
+
+func bucketExists(bucketName string) bool {
+	file, err := os.Open("buckets.csv")
+	if err != nil {
+		// If file doesn't exist, it means there are no buckets yet
+		if errors.Is(err, os.ErrNotExist) {
+			return false
+		}
+		fmt.Printf("Failed to open metadata file: %v\n", err)
+		return false
+	}
+	defer file.Close()
+
+	reader := csv.NewReader(file)
+	for {
+		record, err := reader.Read()
+		if err != nil {
+			break
+		}
+		if record[0] == bucketName {
+			return true
+		}
+	}
+	return false
+}
+
+// saveBucketMetadata saves the bucket metadata to a CSV file.
+func saveBucketMetadata(bucketName string) error {
+	file, err := os.OpenFile("buckets.csv", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	writer := csv.NewWriter(file)
+	defer writer.Flush()
+
+	// Write metadata entry for the new bucket
+	creationTime := time.Now().Format(time.RFC3339)
+	record := []string{bucketName, creationTime}
+	return writer.Write(record)
 }
--- server.go
+++ server.go
@@ -8,9 +8,9 @@ import (
 
 func StartServer(port string) {
 	err := http.ListenAndServe(":"+port, nil)
+
+	fmt.Printf("Server is running on port %s\n", port)
 	if err != nil {
 		log.Fatalf("Could not start the server: %v", err)
-	} else {
-		fmt.Printf("Server is running on port %s\n", port)
 	}
 }
